b2f437a54375fd79229cd8bedd30b598b220f63b
kss <kss@cubebite.com>
2017-11-02 13:35:12 +0900

    support ps uxc

--- test/node_process_for_test.js
+++ test/node_process_for_test.js
@@ -1,10 +1,31 @@
-var now = Date.now();
-console.log('[child] child process start!');
+var ps = require('../index.js');
 
-function doSomething() {
-  return null;
-}
+// A simple pid lookup
+ps.lookup({
+  command: '',
+  psargs: 'uxc'
+}, function(err, resultList ) {
+  if (err) {
+    throw new Error( err );
+  }
 
-setInterval(function () {
-  doSomething();
-}, 50);
+  console.log("USER \t PID \t %CPU \t %MEM \t VSZ \t RSS \t TTY \t STAT \t START \t TIME \t COMMAND");
+  console.log(resultList);
+
+
+
+
+  /*
+  resultList.forEach(function( process ){
+    if( process ){
+      console.log("%s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \t %s \t %s",
+                  process.user, process.pid, process.cpu, process.mem, process.vsz, process.rss, process.tty, process.stat, process.start, process.time, process.command);
+
+      //console.log( 'PID: %s, COMMAND: %s, CPU: %s, USER: %s', process.pid, process.command, process.cpu, process.user);
+      //console.log("USER \t PID \t %CPU \t %MEM \t VSZ \t RSS \t TTY \t STAT \t START \t TIME \t COMMAND");
+
+
+    }
+  });
+  */
+});
